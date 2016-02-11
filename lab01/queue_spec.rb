e8887144eb493ee5688acaf4773c0fb4e88bf3c0
student <say005@client.clab.mephi.ru>
2016-02-11 15:39:04 +0300

    First commit

---
+++ lab01/queue_spec.rb
@@ -0,0 +1,52 @@
+require 'rspec'
+require_relative './queue'
+
+describe Queue do
+
+  let(:queue) { Queue.new }
+
+  context "Свойства методов empty? и empty:" do
+    it "только что созданный стек является пустым" do
+      expect(queue.empty?).to be true
+    end
+
+    it "пустой стек с добавленным в него элементом не является пустым" do
+      queue.enqueue(17)
+      expect(queue.empty?).to be false
+    end
+
+    it "после вызова метода empty очередь становится пустой" do
+      queue. enqueue(17)
+      queue.empty
+      expect(queue.empty?).to be true
+    end
+  end
+
+  context "Особенности работы с пустой и полностью заполненной очередью:" do
+    it "из пустой очереди нельзя ничего извлечь" do
+      expect{queue.dequeue}.to raise_error(RuntimeError, "Queue is empty")
+    end
+
+    it "у пустого стека ничего не лежит на вершине" do
+      expect{queue.front}.to raise_error(RuntimeError, "Queue is empty")
+    end
+
+    it "в пустую очередь можно положить лишь ограниченное число элементов" do
+      queue::DEF_SIZE.times{queue.enqueue(17)}
+      expect{queue.enqueue(17)}.to raise_error(RuntimeError, "Queue is full")
+    end
+  end
+  context " особенности кольцевой реализации"  do
+    it "TODO!"  
+  end
+  context "Особенности дисциплины обслуживания FIFO:" do
+    it "первым пришёл - последним ушёл" do
+      queue.enqueue(17)
+      queue.enqueue(18)
+      expect(queue.front).to eq 17
+      expect(queue.dequeue).to eq 17
+      expect(queue.front).to eq 18
+      expect(queue.dequeue).to eq 18
+    end
+  end
+end
